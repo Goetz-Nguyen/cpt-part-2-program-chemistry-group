0ecff637d889f7b88e9c590576d4db91ac6353bb
Mo6769 <theoneandonlyoxenfree@gmail.com>
2025-12-17 13:30:43 -0500

    Adding compounds
    Compound database

--- compounds.py
+++ compounds.py
@@ -1,5 +1,17 @@
-
 # Compound database
-methane = {"Compound Name": "methane", "Number of Atoms": 5, "Type of Compound": "Organic (Alkane)", "Enthalpy of Formation": -74.4}
-carbon_dioxide = {"Compound Name": "carbon dioxide", "Number of Atoms": 3, "Type of Compound": "Covalent", "Enthalpy of Formation": -393.5}
-ammonia = {"Compound Name": "ammonia", "Number of Atoms": 4, "Type of Compound": "Covalent", "Enthalpy of Formation": -45.9}
+compounds = {
+        #   {COMPOUND NAME}: [{NUMBER OF ATOMS}, {TYPE OF COMPOUND}, {ENTHALPY OF FORMATON}]
+            "Methane": [5, "Organic (Alkane)", -74.4],
+            "Ethane": [8, "Organic (Alkane)", -84.68],
+            "Benzene": [12, "Aromatic (Hydrocarbon)", +49.1],
+            "Carbon Dioxide": [3, "Covalent", -393.5],
+            "Carbon Monoxide": [2, "Covalent", -110.53],
+            "Ammonia": [4, "Covalent", -45.9],
+            "Hydrochloric Acid": [3, "Molecular", -92.3],
+            "Silver Bromide": [2, "Ionic", -100.37],
+            "Glucose": [24, "Monosacchride", -1273.3],
+            "Silver Chloride": [2, "Ionic", -127.1],
+            "Aluminum Chloride": [4, "Covalent", -704],
+            "Calcium Oxide": [2, "Ionic", -635],
+
+            }
