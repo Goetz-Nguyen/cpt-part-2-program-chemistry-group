80e0cbc41af8adc547f072ef8c9c7a0aec293086
Aidan-Lone <aidanlone@hotmail.com>
2025-12-17 14:18:54 -0800

    Chem Stuff Backend Modification

--- backend.py
+++ backend.py
@@ -7,49 +7,40 @@ from elements import *
 from element_groups import *
 
 # Decomposition Function
-def decomposition(first_compound: str, second_compound: str) -> str:
+def _decomposition(independent_element: str, second_compound: str) -> list:
 
-    individual_elements = first_compound.split("-")
     secondary_individual_elements = second_compound.split("-")
 
-    return individual_elements, secondary_individual_elements
+    return independent_element, secondary_individual_elements
 
-# Reacting two compounds together
-def react(first_compound: str, second_compound: str, alkali_metal_list: list, alkaline_earth_metal_list: list, halogen_list: list) -> str:
-    element_list, second_element_list = decomposition(first_compound, second_compound)
+def identify_element_group(independent_element: str, second_compound: str, alkali_metal_list: list, alkaline_earth_metal_list: list, halogen_list: list) -> Element:
+    individual_element, second_element_list = _decomposition(independent_element, second_compound)
 
-# Converting classes for the first compound
+# Converting classes for the independent element
 
     # Checks if the element is an alkali metal
     for element in alkali_metal_list:
-        if element_list[0] in element:
+        if individual_element in element:
             first_element = Alkali_Metal(element[0], element[1], element[2], element[3])
             break
         else:
-            element_list[0] = element_list[0]
+            individual_element = individual_element
 
     # Checks if the element is an alkaline earth metal
     for element in alkaline_earth_metal_list:
-        if element_list[0] in element:
+        if individual_element in element:
             first_element = Alkaline_Earth_Metal(element[0], element[1], element[2], element[3])
             break
         else:
-            element_list[0] = element_list[0]
+            individual_element = individual_element
 
-    # Checks if the element is a halogen
-    for element in halogen_list:
-        if element_list[1] in element:
-            second_element = Halogen(element[0], element[1], element[2], element[3])
-            break
-        else:
-            element_list[1] = element_list[1]
 
 # Converting classes for the second compound
 
     # Checks if the element is an alkali metal
     for element in alkali_metal_list:
         if second_element_list[0] in element:
-            third_element = Alkali_Metal(element[0], element[1], element[2], element[3])
+            second_element = Alkali_Metal(element[0], element[1], element[2], element[3])
             break
         else:
             second_element_list[0] = second_element_list[0]
@@ -57,7 +48,7 @@ def react(first_compound: str, second_compound: str, alkali_metal_list: list, al
     # Checks if the element is an alkaline earth metal
     for element in alkaline_earth_metal_list:
         if second_element_list[0] in element:
-            third_element = Alkaline_Earth_Metal(element[0], element[1], element[2], element[3])
+            second_element = Alkaline_Earth_Metal(element[0], element[1], element[2], element[3])
             break
         else:
             second_element_list[0] = second_element_list[0]
@@ -65,19 +56,34 @@ def react(first_compound: str, second_compound: str, alkali_metal_list: list, al
     # Checks if the element is a halogen
     for element in halogen_list:
         if second_element_list[1] in element:
-            fourth_element = Halogen(element[0], element[1], element[2], element[3])
+            third_element = Halogen(element[0], element[1], element[2], element[3])
             break
         else:
             second_element_list[1] = second_element_list[1]
 
-    if isinstance(first_element, Alkali_Metal) and isinstance(third_element, Alkaline_Earth_Metal):
-        return True
-    else:
-        return False
-
+    return individual_element, second_element_list, first_element, second_element, third_element
+            
+# Reacting two compounds together
+def react(independent_element: str, second_compound: str) -> str:
     
-
+    individual_element, second_element_list, first_element, second_element, third_element = identify_element_group(
+                                                                                independent_element,
+                                                                                second_compound,
+                                                                                alkali_metal_list, 
+                                                                                alkaline_earth_metal_list, 
+                                                                                halogen_list
+                                                                                        )
     
+    if isinstance(first_element, Element) and isinstance(second_element, Element):
+        if activity_series.index(first_element.name) > activity_series.index(second_element.name):
+            first_element.name, second_element_list[0] = second_element_list[0], first_element.name
+        else:
+            print(f"Reaction cannot occur; {first_element.name} is lower on the activity series of" \
+                  f" metals, compared to {second_element.name}.")
 
-print(react("Na-F", "Mg-Cl", alkali_metal_list, alkaline_earth_metal_list, halogen_list))
+    return first_element.name, second_element_list
+            
+
+                                                                                    
+print(react("Mg", "Na-Cl"))
 
