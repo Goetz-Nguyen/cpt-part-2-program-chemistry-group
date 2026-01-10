43e61be1b9020ef7401ded95aa99786087394ad2
Malaz1512 <malazimad2008@gmail.com>
2026-01-10 12:38:19 -0500

    Actually correct menu and organic_compound_generator code
    The other menu code was just so i can see suggestions from my classmates, this is the correct code with all functional implementations and suggestions and correctly formatted code (no gazillion enters), also organic compound generator was CLI still, we forgot to push the non-cli version yesterday so i took the liberty of pushing it today

--- organic_compound_generator.py
+++ organic_compound_generator.py
@@ -5,64 +5,31 @@ def organic_compound_generator():
 
 
     prefix_dictionary = {1: "Meth", 2: "Eth", 3: "Prop", 4: "But", 5: "Pent", 6: "Hex", 7: "Hept", 8: "Oct", 9 : "Non", 10: "Dec"}
-
-
-    print("Welcome to the Organic Compound Generator!\n")
-    print("Organic compounds are chemical compounds composed primarily of carbon and hydrogen.")
-    print("The relationship between the two elements is what we will be exploring here!")
    
-    continuous_loop = 0
+    organic_compound_type = int(input("Please enter the number corresponding to the organic compound type you would like to produce: "))
 
+    carbon_chain_length = int(input("How many carbons would you like in your organic compound? (1-10): "))
 
-    while continuous_loop == 0:
-        try:
-            print("----\n")
-            print("1. Alkane (Contains only single-bonded carbon-carbon chains)")
-            print("2. Alkene (Contains an instance of a double-bonded carbon-carbon chain)")
-            print("3. Alkyne (Contains an instance of a triple-bonded carbon-carbon chain)")
-            print("----\n")
-            organic_compound_type = int(input("Please enter the number corresponding to the organic compound type you would like to produce: "))
+    if organic_compound_type == 1:
+        chemical_formula = f"C{carbon_chain_length}H{(2 * carbon_chain_length) + 2}"
+        chemical_name = f"{prefix_dictionary[carbon_chain_length]}ane"
+    elif organic_compound_type == 2:
+        chemical_formula = f"C{carbon_chain_length}H{(2 * carbon_chain_length)}"
+        chemical_name = f"{prefix_dictionary[carbon_chain_length]}ene"
+    elif organic_compound_type == 3:
+        chemical_formula = f"C{carbon_chain_length}H{(2 * carbon_chain_length) - 2}"
+        chemical_name = f"{prefix_dictionary[carbon_chain_length]}yne"
 
+    now = dt.now()
+    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    if os.path.isfile("history.txt"):
+        with open("history.txt", "a") as f:
+            f.write(f"User created {chemical_name} ({chemical_formula}) - {dt_string}\n")
+    else:
+        with open("history.txt", "w") as f:
+            f.write(f"User created {chemical_name} ({chemical_formula}) - {dt_string}\n")
 
-            while (organic_compound_type < 1) or (organic_compound_type > 3):
-                print("That is not an available option; please try again.")
-                organic_compound_type = int(input("Please enter the number corresponding to the organic compound type you would like to produce: "))
-
-
-            carbon_chain_length = int(input("How many carbons would you like in your organic compound? (1-10): "))
-
-
-            while (carbon_chain_length < 1) or (carbon_chain_length > 10):
-                print("That is not an available option; please try again.")
-                carbon_chain_length = int(input("How many carbons would you like in your organic compound? (1-10): "))
-
-
-            if organic_compound_type == 1:
-                chemical_formula = f"C{carbon_chain_length}H{(2 * carbon_chain_length) + 2}"
-                chemical_name = f"{prefix_dictionary[carbon_chain_length]}ane"
-            elif organic_compound_type == 2:
-                chemical_formula = f"C{carbon_chain_length}H{(2 * carbon_chain_length)}"
-                chemical_name = f"{prefix_dictionary[carbon_chain_length]}ene"
-            elif organic_compound_type == 3:
-                chemical_formula = f"C{carbon_chain_length}H{(2 * carbon_chain_length) - 2}"
-                chemical_name = f"{prefix_dictionary[carbon_chain_length]}yne"
-
-
-            print(f"Chemical Formula: {chemical_formula}")
-            print(f"Chemical Formula: {chemical_name}")
-
-            now = dt.now()
-            dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-            if os.path.isfile("history.txt"):
-                with open("history.txt", "a") as f:
-                    f.write(f"User created {chemical_name} - {dt_string}\n")
-            else:
-                with open("history.txt", "w") as f:
-                    f.write(f"User created {chemical_name} - {dt_string}\n")
-
-        except ValueError:
-            print("Invalid input; please try again.")
-
+    return chemical_formula, chemical_name
 
 if __name__ == "__main__":
     organic_compound_generator()
